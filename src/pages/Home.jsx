06412078288fd64f4ce307cf26eaa19358b9155f
Long Lane <tech@long-lane.co.uk>
2023-10-12 22:30:15 +0100

    add infinite scrolling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,49 @@
 import react, { useEffect, useState } from "react";
-import { Card } from '../components/Card';
+import { Card } from "../components/Card";
 
 export const Home = () => {
-  const [animeData, setAnimeData] = useState([]);
-  const getAnimeData = async () => {
-    const data = await fetch("https://kitsu.io/api/edge/characters", {
-      method: "GET",
-    });
-    const payload = await data.json();
-    setAnimeData(payload.data);
+  const [animeData, setAnimeData] = useState({ data: [], links: [], meta: [] });
+  const getAnimeData = async (url = "https://kitsu.io/api/edge/characters") => {
+    try {
+      const data = await fetch(url, {
+        method: "GET",
+      });
+      const payload = await data.json();
+      setAnimeData({
+        data: [...animeData.data, ...payload.data],
+        meta: payload.meta,
+        links: payload.links,
+      });
+      console.log(animeData);
+    } catch (error) {
+      console.log(error);
+    }
   };
+  const [bottomPage, setBottomPage] = useState(false);
+
   useEffect(() => {
     getAnimeData();
   }, []);
 
-  return(animeData.length > 1 &&
-    
-    <section className='card-wrapper'>
-    {animeData.map((entry) => {
-      return <Card props={entry} key={`character-${entry.id}`}/>;
-    })}
-    
-    </section>
+  useEffect(() => {
+    bottomPage && getAnimeData(animeData.links.next);
+  }, [bottomPage]);
+  {
+    window.addEventListener("scroll", () => {
+      setBottomPage(
+        window.innerHeight + document.documentElement.scrollTop >=
+          document.documentElement.offsetHeight
+      );
+    });
+  }
+
+  return (
+    animeData.data.length > 1 && (
+      <section className="card-wrapper">
+        {animeData.data.map((entry) => {
+          return <Card props={entry} key={`character-${entry.id}`} />;
+        })}
+      </section>
     )
+  );
 };
